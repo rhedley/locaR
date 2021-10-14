895d5979bbcd9c25922a735ac705e809a66a544b
Richard Hedley <rhedley@ualberta.ca>
2021-10-14 15:13:54 -0600

    Update DetectionSpectrograms_49_2021-08-12.R

--- R/DetectionSpectrograms_49_2021-08-12.R
+++ R/DetectionSpectrograms_49_2021-08-12.R
@@ -1,7 +1,7 @@
 
 
 
-#layoutMatrix#### 
+#layoutMatrix####
 
 #layoutMatrix creates a matrix of station names, which correspond to the layout of stations in space.
 #This is passed to the omniSpectro function for the purposes of generating spectrograms that align
@@ -17,32 +17,32 @@
 #Should change this to include an "auto" function.
 layoutMatrix = function(st, start=c('topleft', 'topright', 'bottomleft', 'bottomright'),
                         byrow=TRUE, nrow, ncol) {
-  
+
   stations = data.frame(station=sort(st$files$Station), row=NA, col=NA, stringsAsFactors=F)
-  
+
   #eight combinations of start (4) and byrow (2)
 
   stations$row = rep(1:nrow, each=ncol) #Top to bottom
   if(start %in% c('bottomleft', 'bottomright')) {stations$row = rev(stations$row)} #reverse if starting from bottom.
   stations$col = rep(1:ncol, times=nrow) #left to right.
   if(start %in% c('topright', 'bottomright')) {stations$col = rev(stations$col)} #reverse if starting from right.
-  
-  
+
+
   m = matrix(NA, nrow=nrow, ncol=ncol)
-  
+
   for(i in 1:nrow(stations)) {
-      m[stations$row[i],stations$col[i]] = stations$station[i] 
+      m[stations$row[i],stations$col[i]] = stations$station[i]
   }
-  
+
   if(!byrow & start %in% c('topleft', 'bottomright')) {m = t(m)}
   if(!byrow & start %in% c('bottomleft', 'topright')) {
     m = m[nrow:1,]
     m = m[,ncol:1]
     m = t(m)
   }
-  
+
   return(m)
-  
+
 }
 
 #lm = layoutMatrix(st, start = 'topleft', byrow=F, nrow=7, ncol=7)
@@ -53,21 +53,17 @@ layoutMatrix = function(st, start=c('topleft', 'topright', 'bottomleft', 'bottom
 #of detecting singing birds within an array.
 
 omniSpectro = function(st, layoutMatrix, intervalLength = 5, intervals = 'all') {
-  
-  require(tuneR)
-  require(seewave)
-  require(oce)
-  
+
   specDir = file.path(st$outputFolder,'Spectrograms')
-  
+
   sl = st$surveyLength
   il = intervalLength
-  
-  output = data.frame(begin=seq(0,sl-il,il), 
+
+  output = data.frame(begin=seq(0,sl-il,il),
                       end=seq(il,sl,il))
-  
+
   output$text=paste0('Sound', formatC(output$begin, width=nchar(sl), flag='0'))
-  
+
   nmics=nrow(st$files)
 
   #mySpectro function.  Note this is not the same as the other mySpectro function! Need to make two
@@ -78,7 +74,7 @@ omniSpectro = function(st, layoutMatrix, intervalLength = 5, intervals = 'all') 
       Channel=ListOfData$channel
       first=ListOfData$first
       last=ListOfData$last
-      imagep(ListOfData$time, ListOfData$freq, t(ListOfData$amp), 
+      imagep(ListOfData$time, ListOfData$freq, t(ListOfData$amp),
              drawPalette=F, ylim=c(0,10), mar=rep(0,4), axes=F,
              breaks=seq(-0,85,length.out=21), col=rev(gray.colors(20, 0,1)))
       legend('topleft', legend=paste0(Mic, ifelse(Channel==2,'b','a')), bty='n', cex=2)
@@ -97,18 +93,18 @@ omniSpectro = function(st, layoutMatrix, intervalLength = 5, intervals = 'all') 
   #make plot of the microphone layout.
   jpeg(paste0(specDir,'/MicLayout.jpeg'), height=7, width=7, units='in', res=200)
   par(mar=c(4,6,1,1))
-  plot(st$files$Easting, st$files$Northing, pch=20, las=1, 
+  plot(st$files$Easting, st$files$Northing, pch=20, las=1,
        xlim=c(min(st$files$Easting)-30, max(st$files$Easting)+30),
        ylim=c(min(st$files$Northing)-30, max(st$files$Northing)+30), xlab='Easting', ylab='Northing')
   text(st$files$Easting, st$files$Northing, labels=st$files$Station, pos=3, cex=0.5)
   dev.off()
 
   #Make a plot of the spectrograms for each sound.
-  
+
   if(sum(intervals == 'all')) {
     intervals=1:nrow(output)
   }
-  
+
   i=1
   j=1
   for(i in intervals) {
@@ -123,46 +119,46 @@ omniSpectro = function(st, layoutMatrix, intervalLength = 5, intervals = 'all') 
       s = t(layoutMatrix)[j]
       #file for that station.
       file = as.character(st$files$Path[st$files$Station==s])
-      
+
       #create sound1
       sound1 = NA
-      
+
       #If there is a sound file, read it.
       if(!is.na(file)) {
         #Get sample rate
         Fs = readWave(file, header=T)$sample.rate
-        
+
         #Get bitrate
         Br = readWave(file, header=T)$bits
-        
+
         #Get time adjustment
         A = st$files$Adjustment[st$files$Station==s]
-        
+
         #Channel
         Ch = st$files$Channel[st$files$Station==s]
-        
+
         #Read appropriate channel from file.
         if(Ch == 1 | is.na(Ch)){
           sound1 = readWave(file, from = first-A, to = last-A, units = 'seconds')@left
         } else {
           sound1 = readWave(file, from = first-A, to = last-A, units = 'seconds')@right
         }
-        
+
         #If there is an adjustment, and this is the first interval, add white noise to beginning.
         #This will bring files with different start times into alignment.
         if(A > 0 & first==0) {
-          sound1=tuneR::bind(noise(kind='white', duration=Fs*A, samp.rate=Fs, bit=Br, pcm=T), 
+          sound1=tuneR::bind(noise(kind='white', duration=Fs*A, samp.rate=Fs, bit=Br, pcm=T),
                       Wave(sound1[1:(length(sound1)-Fs*A)], samp.rate=Fs, bit=Br, pcm=T))
         }
         #Spectrogram
         sound1 = spectro(sound1, f=Fs,  plot=F, ovlp=0, norm=F)
-        
+
         sound1$mic=st$files$Station[st$files$Station==s]
         sound1$channel=st$files$Channel[st$files$Station==s]
         sound1$first=first
         sound1$last=last
       }
-      
+
       #combine into list.
       if(j==1) {
         soundList=list(a=sound1)
@@ -170,7 +166,7 @@ omniSpectro = function(st, layoutMatrix, intervalLength = 5, intervals = 'all') 
         soundList=append(soundList, list(a=sound1))
       }
     }
-    
+
     #Create jpeg.
     jpeg(paste0(specDir, '/',output$text[i], '.jpeg'), width=30, height=15, units='in', res=200)
       par(mfrow=c(nrow(layoutMatrix),ncol(layoutMatrix)))
